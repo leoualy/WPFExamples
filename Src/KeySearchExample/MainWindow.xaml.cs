49a2399a8af7b0ebc8b82aad88ac9a7fc38251ef
nauy <d@icloud.com>
2018-05-17 16:38:57 +0800

    add KeySearchExample

---
+++ Src/KeySearchExample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeySearchExample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            model = new ViewModel();
+            this.DataContext = model;
+        }
+
+
+    }
+}
---
+++ Src/KeySearchExample/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace KeySearchExample
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        List<string> list;
+        public ViewModel()
+        {
+            ViewSource = new CollectionViewSource();
+             list=new List<string>(){
+                "h","helo","height","move"
+
+            };
+
+             ViewSource.Source = list;
+            ViewSource.Filter += ViewSource_Filter;
+        }
+
+        void ViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextFilter))
+            {
+                e.Accepted = list.Contains(TextFilter);
+                Console.WriteLine(TextFilter);
+            }
+
+        }
+        public CollectionViewSource ViewSource { get; set; }
+
+        string mTextFilter;
+        public string TextFilter
+        {
+            get { return mTextFilter; }
+            set
+            {
+                mTextFilter = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("TextFilter"));
+                }
+                ViewSource.View.Refresh();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
