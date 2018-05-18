60ffd5b608ac21dc3399b709956b814d4dad5ea0
nauy <d@icloud.com>
2018-05-18 14:00:39 +0800

    add CommandExample

---
+++ Src/CommandExample/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandExample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MsgCommand MsgCommand { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            MsgCommand = new MsgCommand();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ Src/CommandExample/MsgCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace CommandExample
+{
+    public class MsgCommand:ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Test Command");
+        }
+    }
+}
